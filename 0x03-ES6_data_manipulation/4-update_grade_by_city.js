e5803998e0abc66d7edd746d2d09080643aa7f4f
CANDELY001 <candely1charifa@gmail.com>
2024-01-08 17:03:42 +0000

    ALX

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,24 @@
+  getListStudents,
+  city,
+  newGrades,
+) {
+  if (!Array.isArray(getListStudents) || !Array.isArray(newGrades)) return [];
+  return getListStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find(
+        (grade) => grade.studentId === student.id,
+      );
+      if (matchingGrade) {
+        return {
+          ...student,
+          grade:
+            matchingGrade.grade !== undefined ? matchingGrade.grade : 'N/A',
+        };
+      }
+      return {
+        ...student,
+        grade: 'N/A',
+      };
+    });
+}
